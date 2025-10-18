a20f63c0f4c2a68e3592a81a59f8f28706776e00
Stevan Sapun <stevan.sapun@gmail.com>
2025-10-18 17:58:49 +0200

    Fixed CEP infinite-looping error.

--- STEVAN_SAPUN_SV7-2021_43/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/CepService.java
+++ STEVAN_SAPUN_SV7-2021_43/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/CepService.java
@@ -30,20 +30,27 @@ public class CepService {
 
         // Insert machine
         Machine m = new Machine("M1", "Pump A");
+        Machine m2 = new Machine("M2", "Pump B");
         ksession.insert(m);
+        ksession.insert(m2);
 
         // Temperature Rule
         ksession.insert(new TemperatureReading("M1", 101.0, new Date()));
+        ksession.insert(new TemperatureReading("M2", 100.0, new Date()));
         clock.advanceTime(10, TimeUnit.SECONDS);
 
         ksession.insert(new TemperatureReading("M1", 103.0, new Date()));
+        ksession.insert(new TemperatureReading("M2", 101.0, new Date()));
         clock.advanceTime(10, TimeUnit.SECONDS);
 
         ksession.insert(new TemperatureReading("M1", 105.0, new Date()));
+        ksession.insert(new TemperatureReading("M2", 103.0, new Date()));
         clock.advanceTime(10, TimeUnit.SECONDS);
 
         // Vibration Rule
         ksession.insert(new VibrationReading("M1", 10.5, new Date()));
+        ksession.insert(new VibrationReading("M2", 10.5, new Date()));
+        //ksession.insert(new TemperatureReading("M2", 103.0, new Date()));
         clock.advanceTime(10, TimeUnit.SECONDS);
 
         ksession.insert(new VibrationReading("M1", 12.0, new Date()));
@@ -58,7 +65,8 @@ public class CepService {
         int fired = ksession.fireAllRules();
 
         System.out.println("Rules fired: " + fired);
-        System.out.println("Machine after CEP: " + m);
+        System.out.println("Machine1 after CEP: " + m);
+        System.out.println("Machine2 afrer CEP: " + m2);
 
         ksession.dispose();
         return m;
